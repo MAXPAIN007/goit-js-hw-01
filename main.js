26fa1871884ff588eeeb0aef023a1ef1b7bb9c54
MAXPAIN007 <krmetalist@gmail.com>
2025-03-02 00:11:15 +0100

    HWJS1 is done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-console.log("Файл main.js загружен!");
-import './js/task-1.js';
-import './js/task-2.js';
-import './js/task-3.js';
+// console.log("Файл main.js загружен!");
+
+// import './js/task-1.js';
+// import './js/task-2.js';
+// import './js/task-3.js';
